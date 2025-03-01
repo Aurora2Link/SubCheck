2282ca757f798076b90e2dc753c3d2292e097bc8
Aurora2Link <aurora2link@gmail.com>
2025-03-01 03:37:57 -0600

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.CharField(max_length=15, primary_key=True)  # Ahora es un string de hasta 15 caracteres
     subscription_until = models.DateField()
 
     def __str__(self):
