09c216e3de5c2bf8818daf4b295bb1d07318fe74
Aurora2Link <aurora2link@gmail.com>
2025-03-01 01:10:26 -0600

    Update settings.py

--- subcheck/settings.py
+++ subcheck/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3c-)(7lljgr%4k@10^)k2mo0s$-(1+xu9@r649l*rnua^wtkpw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
